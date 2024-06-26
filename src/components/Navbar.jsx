6468869dc446440ce812274d6684fdc1b6d3e95a
Jan Michael Caasi <janmichaelcaasi@gmail.com>
2024-06-26 10:46:52 +0800

    reload fix2?

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -126,33 +126,33 @@ const Navbar = ({handleSubmitLogin}) => {
   }, [])
 
   // Upload Pic
-  const uploadProfileImage = async (e) => {
-    e.preventDefault()
-    const fileInput = e.target.elements.image
+  // const uploadProfileImage = async (e) => {
+  //   e.preventDefault()
+  //   const fileInput = e.target.elements.image
 
-    if (!fileInput.value) {
-      apiError('Please select a file to upload')
-      return
-    }
+  //   if (!fileInput.value) {
+  //     apiError('Please select a file to upload')
+  //     return
+  //   }
 
-    const formData = new FormData()
-    formData.append('image', fileInput.files[0])
+  //   const formData = new FormData()
+  //   formData.append('image', fileInput.files[0])
 
-    try {
-      const res = await api.post(`/upload/${userId}`, formData, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-      apiSuccess(res.data.message)
-      fetchDefaultProfile()
-      fileInput.value = ''
+  //   try {
+  //     const res = await api.post(`/upload/${userId}`, formData, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         'Content-Type': 'multipart/form-data'
+  //       }
+  //     })
+  //     apiSuccess(res.data.message)
+  //     fetchDefaultProfile()
+  //     fileInput.value = ''
 
-    } catch (error) {
-      apiError(error.message)
-    }
-  }
+  //   } catch (error) {
+  //     apiError(error.message)
+  //   }
+  // }
 
   //logout
   const logout = async () => {
