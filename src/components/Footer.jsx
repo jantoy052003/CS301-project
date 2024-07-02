7e131efc21c490cf1df6f3e31a9bf4d4168fe5b8
Jan Michael Caasi <janmichaelcaasi@gmail.com>
2024-07-02 19:21:09 +0800

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,22 @@ import { NavLink, Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <>
-      <div className='text-gray-200 bg-gray-700 flex flex-col lg:flex lg:justify-between lg:items-center gap-2 px-10 pt-5'>
+      <div className='mb-0 text-gray-200 bg-gray-700 flex flex-col lg:flex lg:justify-between lg:items-center gap-2 px-10 pt-5'>
         <div className='flex flex-col items-center lg:flex lg:justify-between lg:items center'>
           <div className='w-16'>
             <img src={assets.tabLogo} alt="" />
           </div>
-          <div className='flex gap-48 lg:gap-80'>
+          <div className='flex flex-col lg:flex-row lg:gap-80'>
               <div className='pt-3'>
-                  <ul className='list-disc lg:flex gap-10'>
+                  <ul className='list-disc flex gap-8 lg:gap-10'>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/menu">Menu</Link></li>
                     <li><Link to="/about">About Us</Link></li>
                     <li><Link to="/reviews">Reviews</Link></li>
                   </ul>
               </div>
-              <div className='flex items-start pt-3'>
-                  <ul className='list-disc lg:flex gap-10'>
+              <div className='flex items-center justify-center pt-3'>
+                  <ul className='list-disc gap-4 flex justify-center lg:gap-10'>
                       <li>+63123-234-231</li>
                       <li>contact@test.com</li>
                   </ul>
@@ -27,7 +27,7 @@ const Footer = () => {
           </div>
         </div>
         <hr />
-        <p className='mx-24 w-full lg:w-fit'>Copyright 2024 © Test.com - All Rights Reserve.</p>
+        <p className='lg:mx-24 w-full lg:w-fit'>Copyright 2024 © Test.com - All Rights Reserve.</p>
       </div>
     </>
   )
